c92dceb7bfb48c2d95c3117273cb3660b4108738
Diana Castaño <dianaczapata@gmail.com>
2023-11-29 00:42:05 -0500

    create forms desktop-version

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RegisterScreen from "./Register"
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className="text-3xl bg-green-300 text-center p-5 text-white">Hello world</h1> */}
+      <RegisterScreen/>
+    </>
+  )
+}
+
+export default App
